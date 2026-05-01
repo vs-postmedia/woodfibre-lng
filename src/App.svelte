1eca17e89a16aee7c70d8ba50331ab1afd18fb1b
Nathan Griffiths <ngriffiths@postmedia.com>
2026-05-01 16:31:13 -0700

    copy edits

--- src/App.svelte
+++ src/App.svelte
@@ -6,19 +6,19 @@
     import fonts from './css/fonts.css';
     import css from './css/main.css';
 
-    // IMAGES
-    import gfx from '/images/map.jpg';
 
     // SET CHART HEIGH & URL HERE...
     const chartId01 = '28786191';
-    const chartHeight = '500px';
+    const chartHeight = (window.innerWidth < 500) ? '300px': '500px';
     const fullWidthId = '28724001'
     const fullWidthHeight = '400px';
+
+    console.log(chartHeight)
 </script>
 
 <!-- MARKUP -->
 <header>
-    <h1>Woodfibre LNG violating waste permit</h1>
+    <h1>FortisBC violating waste permit</h1>
     <p class="subhead">For more than a year, a water treatment plant at the WoodFibre LNG facility near Squamish has been discharging effluent with higher than allowed levels of copper and aluminum into a creek that feeds into Átl’ka7tsem/Howe Sound, a UNESCO biosphere.</p>
 </header>
 
@@ -35,7 +35,7 @@
 
 <footer>
     <p class="source">Source:
-        <a href="https://talkingenergy.ca/water-quality-testing-results" target="_blank">Fortis B.C.</a>
+        <a href="https://talkingenergy.ca/water-quality-testing-results" target="_blank">FortisBC</a>
     </p>
     <!-- flourish logo -->
     <div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/3706064/?utm_source=embed&utm_campaign=visualisation/3706064' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
