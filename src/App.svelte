13f86f03ee55e057ede229a1e3ccd72c03d2b7dd
Nathan Griffiths <ngriffiths@postmedia.com>
2026-05-01 09:54:11 -0700

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -10,23 +10,21 @@
     import gfx from '/images/map.jpg';
 
     // SET CHART HEIGH & URL HERE...
-    const chartId01 = '5258140';
-    const chartId02 = '5258446';
-    const chartHeight = '375px';
-    const fullWidthId = '25071549'
-    const fullWidthHeight = '300px';
+    const chartId01 = '28786191';
+    const chartHeight = '500px';
+    const fullWidthId = '28724001'
+    const fullWidthHeight = '400px';
 </script>
 
 <!-- MARKUP -->
 <header>
-    <h1>HED</h1>
-    <p class="subhead">TK TK TK</p>
+    <h1>Woodfibre LNG violating waste permit</h1>
+    <p class="subhead">For more than a year, a water treatment plant at the WoodFibre LNG facility near Squamish has been discharging effluent with higher than allowed levels of copper and aluminum into a creek that feeds into Átl’ka7tsem/Howe Sound, a UNESCO biosphere.</p>
 </header>
 
 <!-- SIDE-BY-SIDE -->
-<div class="graphic-container">
+<div class="graphic-container full-width">
     <iframe src='https://flo.uri.sh/visualisation/{chartId01}/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:{chartHeight};' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
-    <iframe src='https://flo.uri.sh/visualisation/{chartId02}/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:{chartHeight};' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
 </div>
 
 <!-- FULL WIDTH -->
@@ -34,17 +32,10 @@
     <iframe src='https://flo.uri.sh/visualisation/{fullWidthId}/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:{fullWidthHeight};' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
 </div>
 
-<!-- IMAGES -->
- <div class="graphic-container">
-    <div class="img-container">
-        <img src={gfx} alt="data visualization"/>
-    </div>
- </div>
 
 <footer>
     <p class="source">Source:
-        <a href="https://www.vancouversun.com/" target="_blank">tk</a>,
-        <a href="https://www.openstreetmap.org/" target="_blank">tk</a>
+        <a href="https://talkingenergy.ca/water-quality-testing-results" target="_blank">Fortis B.C.</a>
     </p>
     <!-- flourish logo -->
     <div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/3706064/?utm_source=embed&utm_campaign=visualisation/3706064' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flourish-scrollytelling-template/',
+  base: '/woodfibre-lng/',
   // base: process.env.MODE === 'production' ? '/09xx-tfwp-story/' : '/',
   plugins: [svelte()],
   resolve: {
